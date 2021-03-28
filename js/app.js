9b6ef194aefa53da3647071a7376929635037275
csw-techi <csw.tech@icloud.com>
2021-03-28 18:02:37 -0400

    update css html & js

--- js/app.js
+++ js/app.js
@@ -1,52 +1,19 @@
 var lincoln = 0
 var frankfort = 0
 var duncan = 0
-// var count = 0
 
-function myFunction() {
-    let lincolntotal = lincoln+=1;
-    document.getElementById("lincoln-button").inputMode = lincoln;
-    alert ("You voted for Lincoln Heritage Scenic Byway" "+lincolntotal+");
-        
-    frankfort+=1;
-    document.getElementById("franfort-button").innerHTML = frankfort;
-    alert ("You voted for Old Frankfort Pike");
-        
-    duncan+=1;
-    document.getElementById("duncan-button").innerHTML = duncan;
-    alert ("You voted for Duncan Hines Scenic Byway");
-    }
-
-
-    // var results = document.getElementById('results');
-// //   results.innerHTML = 'total: ' + (lincoln + frankfort + duncan);
-//   results.innerHTML += '<br />trump: ' + lincoln;
-//   results.innerHTML += '<br />hillary: ' + frankfort;
-//   results.innerHTML += '<br />hillary: ' + duncan;
-// }
-
-// document.getElementById('lincoln-button').addEventListener('onclick', function () {
-//   lincoln++;
-//   refreshResults();
-// });
-
-// document.getElementById('frankfort-button').addEventListener('onclick', function () {
-//   frankfort++;
-//   refreshResults();
-// });
-// document.getElementById('duncan-button').addEventListener('onclick', function () {
-//     duncan++;
-//     refreshResults();
-// });
-
-// window.alert (results);
-
-
-
-// var count = 0;
-// function myFunction() {
-//     count+=1;
-//     document.getElementById("btn").innerHTML = count;
-//     alert (count);
-    
-// }
+function myFunction1() {
+    let lincolntotal = lincoln++;
+    document.getElementById("lincolntotal").innerHTML = lincoln;
+    alert("You voted for Lincoln Heritage Scenic Byway");
+}
+function myFunction2() {
+    let frankforttotal = frankfort++;
+    document.getElementById("frankforttotal").innerHTML = frankfort;
+    alert("You voted for Old Frankfort Pike");
+}
+function myFunction3() {
+    let duncantotal = duncan++;
+    document.getElementById("duncantotal").innerHTML = duncan;
+    alert("You voted for Duncan Hines Scenic Byway");
+}
