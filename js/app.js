c7864e0c95379d26fe7f1ee6040dcb075790e008
csw-techi <csw.tech@icloud.com>
2021-03-29 17:41:29 -0400

    update css html & js - almost final

--- js/app.js
+++ js/app.js
@@ -5,15 +5,15 @@ var duncan = 0
 function myFunction1() {
     let lincolntotal = lincoln++;
     document.getElementById("lincolntotal").innerHTML = lincoln;
-    alert("You voted for Lincoln Heritage Scenic Byway");
+    alert("You voted for Lincoln Heritage Scenic Byway!");
 }
 function myFunction2() {
     let frankforttotal = frankfort++;
     document.getElementById("frankforttotal").innerHTML = frankfort;
-    alert("You voted for Old Frankfort Pike");
+    alert("You voted for Old Frankfort Pike!");
 }
 function myFunction3() {
     let duncantotal = duncan++;
     document.getElementById("duncantotal").innerHTML = duncan;
-    alert("You voted for Duncan Hines Scenic Byway");
+    alert("You voted for Duncan Hines Scenic Byway!");
 }
